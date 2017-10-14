5081ea7b93f59de4551fee8eeb20f37d70fa6fc2
Politov Artyom <politovartem@gmail.com>
2017-10-14 17:22:21 +0300

    Begin implementing of DataAccess: ClientProvider

---
+++ IntegrationTests/OauthService.DataAccess/ClientProviderTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OauthService.DataAccess;
+using OauthService.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.OauthService.DataAccess
+{
+    [TestClass]
+    public class ClientProviderTests
+    {
+        private static string _testConnectionString = @"Data Source=C:\Temp\IdentityServer.sqlite";
+
+        [TestMethod]
+        public void Ctor_checks_arguments()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => { var provider = new ClientProvider(" "); });
+            Assert.ThrowsException<ArgumentNullException>(() => { var provider = new ClientProvider(""); });
+            Assert.ThrowsException<ArgumentNullException>(() => { var provider = new ClientProvider(null); });
+        }
+
+        [TestMethod]
+        public void Can_get_all_clients()
+        {
+            var provider = new ClientProvider(_testConnectionString);
+            var clients = provider.GetClientsAsync().Result.ToList();
+
+            Assert.IsNotNull(clients);
+            Assert.IsTrue(clients.Any());
+            CollectionAssert.AllItemsAreNotNull(clients);
+        }
+
+        [TestMethod]
+        public void Can_get_client_byId()
+        {
+            var provider = new ClientProvider(_testConnectionString);
+            var clients = provider.GetClientsAsync().Result.ToList();
+
+            var clientId = clients.FirstOrDefault().Id;
+            var client = provider.GetClientAsync(clientId);
+
+            Assert.IsNotNull(client);
+        }
+
+        [TestMethod]
+        public void Cant_add_with_dublicated_identifier()
+        {
+            var provider = new ClientProvider(_testConnectionString);
+            var clients = provider.GetClientsAsync().Result.ToList();
+
+            var newClient = new Client()
+            {
+                Name = "dublicate test",
+                Identifier = clients.First().Identifier
+            };
+
+            Assert.ThrowsException<AggregateException>(() => { var t = provider.AddClientAsync(newClient).Result; });
+        }
+
+        [TestMethod]
+        public void Can_add_new_client()
+        {
+            var provider = new ClientProvider(_testConnectionString);
+            
+            var newClient = new Client()
+            {
+                Name = "create new client test",
+                Identifier = Guid.NewGuid().ToString("R")
+            };
+
+            var newClientId = provider.AddClientAsync(newClient).Result;
+
+            var result = provider.GetClientAsync(newClientId);
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ OauthService.DataAccess/ClientProvider.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using OauthService.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OauthService.DataAccess
+{
+    public class ClientProvider
+    {
+        private string _connectionString;
+
+        public ClientProvider(string connectionString)
+        {
+            if(string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+            _connectionString = connectionString;
+        }
+
+        public async Task<Client> GetClientAsync(long id)
+        {
+            using(var connection = new SqliteConnection(_connectionString))
+            {
+                return await connection.QuerySingleOrDefaultAsync<Client>("SELECT * FROM Clients WHERE Id = @id", new { id = id });
+            }
+        }
+
+        public async Task<Client> GetClientAsync(string identifier)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                return await connection.QuerySingleOrDefaultAsync<Client>("SELECT * FROM Clients WHERE identifier = @identifier", new { identifier = identifier });
+            }
+        }
+
+        public async Task<IEnumerable<Client>> GetClientsAsync()
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                return await connection.QueryAsync<Client>("SELECT * FROM Clients");
+            }
+        }
+
+        public async Task<long> AddClientAsync(Client client)
+        {
+            if (await GetClientAsync(client.Identifier) != null)
+            {
+                throw new DuplicateNameException($"Client with identifier={client.Identifier} already exists");
+            }
+   
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                return await connection.QueryFirstAsync<long>(
+                "INSERT INTO Clients VALUES (null, @Id, @Name, @Identifier, @Secret, @Callback, @Desciption, @LogoutPage)",
+                 client);
+            }
+        }
+
+
+    }
+}
